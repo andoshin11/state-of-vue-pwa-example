4f9dd50199ddf89630d6132d89b757a911a2239a
andoshin11 <shinglish11@gmail.com>
2018-03-04 16:01:33 +0900

    Add runtimeCaching options

--- vue.config.js
+++ vue.config.js
@@ -1,8 +1,18 @@
 module.exports = {
   pwa: {
-    // workboxOptions: {
-    //   runtimeCaching: [{
-    //   }]
-    // }
+    workboxOptions: {
+      runtimeCaching: [
+        {
+          urlPattern: /^https:\/\/api.github.com.*/,
+          handler: "networkFirst",
+          options: {
+            cacheName: "apiResponse",
+            expiration: {
+              maxAgeSeconds: 60 * 60 * 24
+            }
+          }
+        }
+      ]
+    }
   }
 };
